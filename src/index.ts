ec6a26c9ebe05194751ef9406c665253303ac5ee
hplwc <zhanghuipeng@moyi365.com>
2020-12-07 19:24:51 +0800

    feat: 开发环境代码实时更新

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('src/index.ts')
+import Koa from 'koa'
+import Router from 'koa-router'
+
+const app = new Koa()
+const router = new Router()
+
+router.get('/', async ctx => {
+  ctx.body = { res: 'hplwc', code: 200 }
+})
+
+app.use(router.routes())
+
+app.listen(3344, () => {
+  console.log('port is already')
+})
